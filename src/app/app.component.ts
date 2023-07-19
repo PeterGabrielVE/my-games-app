ae923252ff0cc1303313b8bb3f17c519bf0258c5
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-07-19 18:51:19 -0400

    ajuste responsive de los filtros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Listado de juegos</h1>
     <app-games-list></app-games-list>
   `,
   styleUrls: ['./app.component.css']
