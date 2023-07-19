ff2f8762f21ff0a5b261b48526eed70e33e19d52
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-07-19 12:51:44 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GamesListComponent } from './games-list/games-list.component';
+import { GameDetailComponent } from './game-detail/game-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: GamesListComponent },
+  { path: 'juego/:id', component: GameDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
