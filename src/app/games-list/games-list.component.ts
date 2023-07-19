4110cc783868a8ca6d96e7fb26f2a9b50a90cb3d
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-07-19 11:27:29 -0400

    crear componente games list

---
+++ src/app/games-list/games-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { GamesService } from '../games.service';
+import { Game } from '../game';
+
+@Component({
+  selector: 'app-games-list',
+  templateUrl: './games-list.component.html',
+  styleUrls: ['./games-list.component.css']
+})
+export class GamesListComponent implements OnInit {
+
+  games: Game[];
+
+  constructor(private gamesService: GamesService) {
+    this.games = [];
+   }
+
+  ngOnInit(): void {
+    this.gamesService.getGames().subscribe(
+      (data: Game[]) => {
+        this.games = data;
+      }
+    );
+  }
+}
