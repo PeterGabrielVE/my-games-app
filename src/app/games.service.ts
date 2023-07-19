5693b5cbe9ccff31dbcd0eb6ff5abb0e29f0c3d7
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-07-19 12:58:52 -0400

    environment

--- src/app/games.service.ts
+++ src/app/games.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GamesService {
 
-  private BASE_URL = 'https://www.freetogame.com/api';
+  private BASE_URL = environment.apiBaseUrl;
 
   constructor(private http: HttpClient) { }
 
