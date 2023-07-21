ddfd43e8f806289b9428783534812649cfe862cb
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-07-20 23:02:36 -0400

    ajustrs

--- src/app/game-detail/game-detail.component.spec.ts
+++ src/app/game-detail/game-detail.component.spec.ts
@@ -1,10 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { Game } from '../game';
+import { GamesService } from '../games.service';
 import { GameDetailComponent } from './game-detail.component';
 
 describe('GameDetailComponent', () => {
   let component: GameDetailComponent;
   let fixture: ComponentFixture<GameDetailComponent>;
+  let gamesService: jasmine.SpyObj<GamesService>;
+
+  beforeEach(async () => {
+    const gamesServiceSpy = jasmine.createSpyObj('GamesService', ['getGame']);
+
+    await TestBed.configureTestingModule({
+      declarations: [ GameDetailComponent ],
+      providers: [
+        { provide: ActivatedRoute, useValue: { snapshot: { paramMap: { get: () => '1' } } } },
+        { provide: GamesService, useValue: gamesServiceSpy }
+      ]
+    })
+    .compileComponents();
+
+    gamesService = TestBed.inject(GamesService) as jasmine.SpyObj<GamesService>;
+  });
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -17,5 +37,55 @@ describe('GameDetailComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get game detail', () => {
+    const game: Game = {
+      id: 1,
+      title: 'Test Game',
+      thumbnail: 'https://example.com/test.jpg',
+      short_description: 'This is a test game.',
+      game_url: 'https://example.com/test-game',
+      genre: 'Test Genre',
+      platform: 'Test Platform',
+      publisher: 'Test Publisher',
+      developer: 'Test Developer',
+      release_date: '2022-01-01',
+      freetogame_profile_url: 'https://example.com/test-game-profile'
+    };
+
+    gamesService.getGame.and.returnValue(of(game));
+
+    component.ngOnInit();
+
+    expect(gamesService.getGame).toHaveBeenCalledWith('1');
+    expect(component.game).toEqual(game);
+  });
+
+  it('should display game detail', () => {
+    component.game = {
+      id: 1,
+      title: 'Test Game',
+      thumbnail: 'https://example.com/test.jpg',
+      short_description: 'This is a test game.',
+      game_url: 'https://example.com/test-game',
+      genre: 'Test Genre',
+      platform: 'Test Platform',
+      publisher: 'Test Publisher',
+      developer: 'Test Developer',
+      release_date: '2022-01-01',
+      freetogame_profile_url: 'https://example.com/test-game-profile'
+    };
+
+    fixture.detectChanges();
+
+    const titleElement = fixture.nativeElement.querySelector('.game-title');
+    expect(titleElement.textContent).toContain('Test Game');
+
+    const thumbnailElement = fixture.nativeElement.querySelector('.game-thumbnail');
+    expect(thumbnailElement.src).toBe('https://example.com/test.jpg');
+
+    const descriptionElement = fixture.nativeElement.querySelector('.game-description');
+    expect(descriptionElement.textContent).toContain('This is a test game.');
   });
 });
--- src/app/games-list/games-list.component.spec.ts
+++ src/app/games-list/games-list.component.spec.ts
@@ -1,15 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { GamesService } from '../games.service';
+import { Game } from '../game';
+import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
 import { GamesListComponent } from './games-list.component';
+import { of } from 'rxjs';
 
 describe('GamesListComponent', () => {
   let component: GamesListComponent;
   let fixture: ComponentFixture<GamesListComponent>;
+  let gamesService: jasmine.SpyObj<GamesService>;
+
+  beforeEach(async () => {
+    const gamesServiceSpy = jasmine.createSpyObj('GamesService', ['getGames']);
+
+    await TestBed.configureTestingModule({
+      declarations: [ GamesListComponent ],
+      providers: [
+        { provide: GamesService, useValue: gamesServiceSpy },
+        { provide: Router, useValue: {} }
+      ]
+    })
+    .compileComponents();
+
+    gamesService = TestBed.inject(GamesService) as jasmine.SpyObj<GamesService>;
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [GamesListComponent]
-    });
     fixture = TestBed.createComponent(GamesListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +35,121 @@ describe('GamesListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display list of games', () => {
+    const games: Game[] = [
+      {
+        id: 1,
+        title: 'Test Game 1',
+        thumbnail: 'https://example.com/test1.jpg',
+        short_description: 'This is a test game 1.',
+        game_url: 'https://example.com/test-game1',
+        genre: 'Test Genre 1',
+        platform: 'Test Platform 1',
+        publisher: 'Test Publisher 1',
+        developer: 'Test Developer 1',
+        release_date: '2022-01-01',
+        freetogame_profile_url: 'https://example.com/test-game1-profile'
+      },
+      {
+        id: 2,
+        title: 'Test Game 2',
+        thumbnail: 'https://example.com/test2.jpg',
+        short_description: 'This is a test game 2.',
+        game_url: 'https://example.com/test-game2',
+        genre: 'Test Genre 2',
+        platform: 'Test Platform 2',
+        publisher: 'Test Publisher 2',
+        developer: 'Test Developer 2',
+        release_date: '2022-02-01',
+        freetogame_profile_url: 'https://example.com/test-game2-profile'
+      }
+    ];
+
+    spyOn(gamesService, 'getGames').and.returnValue(of(games));
+
+    component.ngOnInit();
+
+    fixture.detectChanges();
+
+    const gameElements = fixture.nativeElement.querySelectorAll('.game-item');
+    expect(gameElements.length).toBe(2);
+
+    const titleElement = gameElements[0].querySelector('.game-title');
+    expect(titleElement.textContent).toContain('Test Game 1');
+
+    const thumbnailElement = gameElements[0].querySelector('.game-thumbnail');
+    expect(thumbnailElement.src).toBe('https://example.com/test1.jpg');
+
+    const descriptionElement = gameElements[0].querySelector('.game-description');
+    expect(descriptionElement.textContent).toContain('This is a test game 1.');
+
+    const genreElement = gameElements[0].querySelector('.game-genre');
+    expect(genreElement.textContent).toContain('Test Genre 1');
+
+    const platformElement = gameElements[0].querySelector('.game-platform');
+    expect(platformElement.textContent).toContain('Test Platform 1');
+  });
+
+  it('should filter games', () => {
+    const games: Game[] = [
+      {
+        id: 1,
+        title: 'Test Game 1',
+        thumbnail: 'https://example.com/test1.jpg',
+        short_description: 'This is a test game 1.',
+        game_url: 'https://example.com/test-game1',
+        genre: 'Test Genre 1',
+        platform: 'Test Platform 1',
+        publisher: 'Test Publisher 1',
+        developer: 'Test Developer 1',
+        release_date: '2022-01-01',
+        freetogame_profile_url: 'https://example.com/test-game1-profile'
+      },
+      {
+        id: 2,
+        title: 'Test Game 2',
+        thumbnail: 'https://example.com/test2.jpg',
+        short_description: 'This is a test game 2.',
+        game_url: 'https://example.com/test-game2',
+        genre: 'Test Genre 2',
+        platform: 'Test Platform 2',
+        publisher: 'Test Publisher 2',
+        developer: 'Test Developer 2',
+        release_date: '2022-02-01',
+        freetogame_profile_url: 'https://example.com/test-game2-profile'
+      }
+    ];
+
+    spyOn(gamesService, 'getGames').and.returnValue(of(games));
+
+    component.ngOnInit();
+
+    gamesService.name = 'Test Game 1';
+    gamesService.genre = 'Test Genre 1';
+    gamesService.platform = 'Test Platform 1';
+
+    component.onSearch();
+
+    fixture.detectChanges();
+
+    const gameElements = fixture.nativeElement.querySelectorAll('.game-item');
+    expect(gameElements.length).toBe(1);
+
+    const titleElement = gameElements[0].querySelector('.game-title');
+    expect(titleElement.textContent).toContain('Test Game 1');
+
+    const thumbnailElement = gameElements[0].querySelector('.game-thumbnail');
+    expect(thumbnailElement.src).toBe('https://example.com/test1.jpg');
+
+    const descriptionElement = gameElements[0].querySelector('.game-description');
+    expect(descriptionElement.textContent).toContain('This is a test game 1.');
+
+    const genreElement = gameElements[0].querySelector('.game-genre');
+    expect(genreElement.textContent).toContain('Test Genre 1');
+
+    const platformElement = gameElements[0].querySelector('.game-platform');
+    expect(platformElement.textContent).toContain('Test Platform 1');
   });
 });
